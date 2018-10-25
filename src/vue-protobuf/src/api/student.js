512700afe07bc0fae390de87a34e255501ac20e7
fengxianqi <fengxianqi@gzhuaying.onaliyun.com>
2018-10-25 18:16:58 +0800

    fix: add remark

--- src/vue-protobuf/src/api/student.js
+++ src/vue-protobuf/src/api/student.js
@@ -1,6 +1,11 @@
 import request from '@/lib/request'
 
+// params是object类型的请求参数
+// school.PBStudentListReq 是定义好的请求体model
+// school.PBStudentListRsp 是定义好的响应model
+// getStudentList 是接口名称
 export function getStudentList (params) {
   const req = request.create('PBStudentListReq', params)
   return request('getStudentList', req, 'school.PBStudentListRsp')
 }
+
