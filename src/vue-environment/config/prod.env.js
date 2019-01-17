94a818f259e2608f042bed61d16dfb9fc3fbf16f
fengxianqi <fengxianqi@gzhuaying.onaliyun.com>
2019-01-17 11:37:44 +0800

    docs: fix

--- src/vue-environment/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  EVN_CONFIG: '"prod"',
-  API_ROOT: '"/api/prod"'
-}
--- src/vue-environment/config/test.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  EVN_CONFIG: '"test"',
-  API_ROOT: '"/api/test"'
-}
