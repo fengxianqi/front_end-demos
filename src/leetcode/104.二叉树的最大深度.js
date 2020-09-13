e2fa1e4b70debc59b055b2ad4be628a43d97b0c3
fengxianqi <609587734@qq.com>
2020-09-13 22:26:56 +0800

    feat: leetcode add 104

---
+++ src/leetcode/104.二叉树的最大深度.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (!root) {
+    return 0
+  }
+  // 子树的max + 1 
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+};
+// @lc code=end
+
