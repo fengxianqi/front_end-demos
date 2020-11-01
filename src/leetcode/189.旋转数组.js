5ce77177039f432accf08bdd6e4a7742b977e249
fengxianqi <609587734@qq.com>
2020-11-01 21:04:51 +0800

    feat: leetcode 189

--- src/leetcode/189.旋转数组.js
+++ src/leetcode/189.旋转数组.js
@@ -5,10 +5,24 @@
  * 
  * 
  * 思路一
- * 截取后半段，然后再续在前面
+ * 暴力一个个遍历， 时间O(k * n)，空间O(1)
+ * 
+ * 思路二 
+ *  时间O(n)，空间O(1)
+ * 1. 翻转数组：[7,6,5,4,3,2,1]
+ * 2. 再翻转前k个，[5,6,7,4,3,2,1]
+ * 3.再翻转后length - k 个： [5,6,7,,1,2,3,4]
  * 
  * 
  * 
+ * 
+ * 思路三
+ * 截取后半段，然后再续在前面，此处要注意要直接改变nums的内容。
+ * 时间O(n)，空间O(k)
+ * 
+ * 思路四
+ * 使用环状替换
+ * 时间O(n)，空间O(1)
  */
 
 // @lc code=start
@@ -18,11 +32,10 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-  const arrK = nums.slice(nums.length - k)
+  const tail = nums.splice(nums.length - k)
+  console.log(tail)
   
-  console.log(arrK)
-  nums = arrK.concat(nums.slice(0, nums.length - k))
-  return nums
+  nums.unshift(...tail)
 };
 // @lc code=end
 
