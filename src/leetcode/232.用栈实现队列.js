79e5960025164d420ebb02e7475a6eb103eae88e
xianqifeng <xianqifeng@digitalgd.com.cn>
2020-11-30 17:04:42 +0800

    feat: leetcode add 232

---
+++ src/leetcode/232.用栈实现队列.js
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=232 lang=javascript
+ *
+ * [232] 用栈实现队列
+ */
+
+// @lc code=start
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function() {
+  this.queue = [];
+};
+
+/**
+ * Push element x to the back of queue. 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+  this.queue.push(x);
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+  return this.queue.shift();
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+  return this.queue[0]
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+  return this.queue.length === 0
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
+// @lc code=end
+
