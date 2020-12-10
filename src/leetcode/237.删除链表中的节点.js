b2c1f356dc3c7a505d89603fb20e213fb3a3043e
fengxianqi <609587734@qq.com>
2020-12-10 23:24:59 +0800

    feat: leetcode add 237

---
+++ src/leetcode/237.删除链表中的节点.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=237 lang=javascript
+ *
+ * [237] 删除链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    // 特地骂一声辣鸡题目，看不懂
+    node.val = node.next.val
+    node.next = node.next.next
+};
+// @lc code=end
+
