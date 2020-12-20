d0a78bb188a22debd7a5509f1e2def111ce1f869
fengxianqi <609587734@qq.com>
2020-12-20 19:21:42 +0800

    feat: leetcode add 258

---
+++ src/leetcode/258.各位相加.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=258 lang=javascript
+ *
+ * [258] 各位相加
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+  // 原数: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+  // 偏移: 0 1 2 3 4 5 6 7 8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 
+  // 取余: 0 1 2 3 4 5 6 7 8  0  1  2  3  4  5  6  7  8  0  1  2  3  4  5  6  7  8  0  1  2  
+  // 数根: 1 2 3 4 5 6 7 8 9  1  2  3  4  5  6  7  8  9  1  2  3  4  5  6  7  8  9  1  2  3 
+  
+  // 作者：windliang
+  // 链接：https://leetcode-cn.com/problems/add-digits/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-5-7/
+  // 来源：力扣（LeetCode）
+  // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+  return Number((num-1)%9)+1
+};
+// @lc code=end
+
