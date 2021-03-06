4addab2c1a266b7058f6292e2538e86bbebb6214
fengxianqi <609587734@qq.com>
2021-03-06 15:52:53 +0800

    feat: leetcode add 215

---
+++ src/leetcode/215.数组中的第k个最大元素.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=javascript
+ *
+ * [215] 数组中的第K个最大元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+  // 升序排序
+  const arr = nums.sort((a, b) => a - b)
+  return arr[nums.length - k]
+};
+// @lc code=end
+
