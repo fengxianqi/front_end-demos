b438e18275881878932e0859014c3c5e56f897b2
fengxianqi <609587734@qq.com>
2021-04-14 00:05:15 +0800

    feat: 二刷21

--- src/leetcode/021.合并两个有序链表.js
+++ src/leetcode/021.合并两个有序链表.js
@@ -19,13 +19,6 @@
 思路： 遍历两个链表
 1. 如果两个链表都不为空，则将小的放到结果，该小的链表指针进入next，进入下一次遍历
 2. 如果其中一个为空，则直接放进结果，进入下一次遍历，直至遍历结束
- */
-
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
 var mergeTwoLists = function(l1, l2) {
     let ret = new ListNode(0)
     const head = ret
@@ -50,6 +43,29 @@ var mergeTwoLists = function(l1, l2) {
       ret = ret.next
     }
     return head.next
+};
+
+
+
+ */
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  if (!l1) {
+      return l2
+  } else if (!l2) {
+      return l1
+  } else if (l1.val < l2.val) {
+      l1.next = mergeTwoLists(l1.next, l2)
+      return l1
+  } else {
+      l2.next = mergeTwoLists(l2.next, l1)
+      return l2
+  }
 };
 // const l1 = new ListNode(1)
 // l1.next = new ListNode(2)
