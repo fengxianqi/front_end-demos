fa540d16a299ef49ce06991327b20e9b0ae94ef5
xianqifeng <xianqifeng@digitalgd.com.cn>
2021-06-30 10:05:45 +0800

    feat: interview 发布订阅模式

---
+++ src/interview/发布订阅模式.js
@@ -0,0 +1,22 @@
+
+// 实现一个发布订阅模式
+
+class EventEmit {
+  // 注册事件的回调函数
+  on(event, callback) {
+    // your code
+  }
+  // 注册事件的回调函数，只执行一次
+  once(event, callback) {
+      // your code
+    }
+  // 触发注册的事件回调函数执行
+  emit(event, ...args) {
+  // your code
+  }
+  // 删除一个回调函数
+  remove(event, callback) {
+  // your code
+  }
+}
+
