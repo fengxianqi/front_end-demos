d35d37e9e5a210bcbc8a8fd6d0d92444bbd88d3f
xianqifeng <xianqifeng@digitalgd.com.cn>
2021-06-30 10:19:39 +0800

    fix: 1

--- src/interview/发布订阅模式.js
+++ src/interview/发布订阅模式.js
@@ -1,22 +1,79 @@
 
 // 实现一个发布订阅模式
 
-class EventEmit {
-  // 注册事件的回调函数
-  on(event, callback) {
-    // your code
+class EventEmitter {
+  constructor() {
+      // 缓存列表
+      this.listener = {}
   }
-  // 注册事件的回调函数，只执行一次
-  once(event, callback) {
-      // your code
-    }
-  // 触发注册的事件回调函数执行
-  emit(event, ...args) {
-  // your code
+
+  // 订阅
+  on(eventName, fn) {
+      // 如果对象中没有对应的 event 值，也就是说明没有订阅过，就给 event 创建个缓存列表
+      // 如有对象中有相应的 event 值，把 fn 添加到对应 event 的缓存列表里
+      if(!this.listener[eventName]){
+          this.listener[eventName] = [];
+      }
+      this.listener[eventName].push(fn);
   }
-  // 删除一个回调函数
-  remove(event, callback) {
-  // your code
+
+  // 取消订阅
+  off(eventName, fn) {
+      let callbacks = this.listener[eventName];
+      // 缓存列表中没有对应的fn，返回false
+      if(!callbacks){
+          return false;
+      }
+      if(!fn){
+          // 如果未传入fn，则将缓存列表中对应的fn都清空
+          callbacks && (callbacks.length = 0);
+      } else {
+          let cb;
+          // 遍历所对应的fn，判断和那个fn相同，相同则删除
+          for (let i = 0, cbLen = callbacks.length; i < cbLen; i++) {
+              cb = callbacks[i];
+              if (cb == fn || cb.fn == fn) {
+                  callbacks.splice(i, 1);
+                  break
+              }
+          }
+      }
+  }
+
+  // 监听一次
+  once(eventName, fn) {
+      // 先绑定，运行时删除对应的值
+      let on = () => {
+          this.off(eventName, on);
+          fn.apply(this, arguments);
+      }
+
+      on.fn = fn;
+      this.on(eventName, on);
+
+  }
+
+  // 发布
+  emit(eventName, data) {
+      const callbacks = this.listener[eventName];
+      if(callbacks) {
+          callbacks.forEach((c) => {
+              c(data);
+          })
+      }
   }
 }
 
+let a = new EventEmitter();
+function aa(x) {
+  console.log(x);
+}
+a.on("kak", aa)
+a.on("kak", (data) => {
+  console.log("1", data);
+})
+
+
+a.emit('kak', 'hahahah');
+a.off('kak',aa);
+a.emit('kak', 'hahahah');
