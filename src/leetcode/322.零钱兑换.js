09dbd49bdf22dbc3ac4d20146ee791958805e8b8
fengxianqi <fengxianqi12@gmail.com>
2021-10-14 00:26:50 +0800

    feat: leetcode add 322

---
+++ src/leetcode/322.零钱兑换.js
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=322 lang=javascript
+ *
+ * [322] 零钱兑换
+ *
+ * https://leetcode-cn.com/problems/coin-change/description/
+ *
+ * algorithms
+ * Medium (44.50%)
+ * Likes:    1525
+ * Dislikes: 0
+ * Total Accepted:    309.8K
+ * Total Submissions: 696.3K
+ * Testcase Example:  '[1,2,5]\n11'
+ *
+ * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+ * 
+ * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+ * 
+ * 你可以认为每种硬币的数量是无限的。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：coins = [1, 2, 5], amount = 11
+ * 输出：3 
+ * 解释：11 = 5 + 5 + 1
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：coins = [2], amount = 3
+ * 输出：-1
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：coins = [1], amount = 0
+ * 输出：0
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：coins = [1], amount = 1
+ * 输出：1
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：coins = [1], amount = 2
+ * 输出：2
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 1 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+  //https://leetcode-cn.com/problems/coin-change/solution/js-xiang-jie-dong-tai-gui-hua-de-si-xiang-yi-bu-da/
+  if(!amount) {
+    return 0;
+}
+
+let dp = Array(amount + 1).fill(Infinity);
+dp[0] = 0;
+
+for(let i =0; i < coins.length; i++) {
+    for(let j = coins[i]; j <= amount; j++) {
+        dp[j] = Math.min(dp[j - coins[i]] + 1, dp[j]);
+    }
+}
+
+return dp[amount] === Infinity ? -1 : dp[amount];
+
+// 作者：carlsun-2
+// 链接：https://leetcode-cn.com/problems/coin-change/solution/dai-ma-sui-xiang-lu-dai-ni-xue-tou-wan-q-80r7/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+};
+// @lc code=end
+
