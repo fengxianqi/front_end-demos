ffd2fc25b12e2258d6a70799d1376a7ec3bda8a1
fengxianqi <fengxianqi12@gmail.com>
2023-03-29 22:22:55 +0800

    feat: leetcode add 240

---
+++ src/leetcode/240.搜索二维矩阵-ii.js
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=240 lang=javascript
+ *
+ * [240] 搜索二维矩阵 II
+ *
+ * https://leetcode.cn/problems/search-a-2d-matrix-ii/description/
+ *
+ * algorithms
+ * Medium (52.56%)
+ * Likes:    1250
+ * Dislikes: 0
+ * Total Accepted:    355.8K
+ * Total Submissions: 676.2K
+ * Testcase Example:  '[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]\n' +
+  '5'
+ *
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ * 
+ * 
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：matrix =
+ * [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],
+ * target = 5
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：matrix =
+ * [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],
+ * target = 20
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= n, m <= 300
+ * -10^9 <= matrix[i][j] <= 10^9
+ * 每行的所有元素从左到右升序排列
+ * 每列的所有元素从上到下升序排列
+ * -10^9 <= target <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+  let m = matrix.length, n = matrix[0].length,i = 0,j=n-1
+  while(i<m&&j>=0){
+      if(matrix[i][j] === target ) return true
+      else if(matrix[i][j] > target){//如果当前元素大于target，那么当前元素下面的一定都比target大，所以左移；
+          j--
+      } else {//如果当前元素小于target，那么当前元素左面的一定都比target小，所以下移；
+          i++
+      }
+  }
+  return false
+};
+// @lc code=end
+
