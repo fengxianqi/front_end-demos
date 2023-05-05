563736e89039f8c2020818abe34ec1227de56836
fengxianqi <fengxianqi12@gmail.com>
2023-05-05 22:46:06 +0800

    feat: leetcode add 347

---
+++ src/leetcode/347.前-k-个高频元素.js
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=347 lang=javascript
+ *
+ * [347] 前 K 个高频元素
+ *
+ * https://leetcode.cn/problems/top-k-frequent-elements/description/
+ *
+ * algorithms
+ * Medium (63.50%)
+ * Likes:    1552
+ * Dislikes: 0
+ * Total Accepted:    423K
+ * Total Submissions: 666K
+ * Testcase Example:  '[1,1,1,2,2,3]\n2'
+ *
+ * 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: nums = [1,1,1,2,2,3], k = 2
+ * 输出: [1,2]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: nums = [1], k = 1
+ * 输出: [1]
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * k 的取值范围是 [1, 数组中不相同的元素的个数]
+ * 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你所设计算法的时间复杂度 必须 优于 O(n log n) ，其中 n 是数组大小。
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+  const map = new Map()
+  for (const item of nums) {
+    if (map.has(item)) {
+      map.set(item, map.get(item)+ 1)
+    } else {
+      map.set(item, 1)
+    }
+  }
+
+  // 转换成二维数组，然后按频率（第二个元素）大到小排序，取前k个即可
+  const list = Array.from(map).sort((a,b) => b[1]-a[1]);
+  return list.slice(0, k).map(i => i[0])
+};
+// @lc code=end
+
