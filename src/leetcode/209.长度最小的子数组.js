2824c3bce6122effe6b7729fc527e8c21b154ef2
fengxianqi <fengxianqi12@gmail.com>
2023-05-12 08:22:42 +0800

    feat: leetcode add 209

---
+++ src/leetcode/209.长度最小的子数组.js
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=javascript
+ *
+ * [209] 长度最小的子数组
+ *
+ * https://leetcode.cn/problems/minimum-size-subarray-sum/description/
+ *
+ * algorithms
+ * Medium (46.94%)
+ * Likes:    1700
+ * Dislikes: 0
+ * Total Accepted:    537.7K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '7\n[2,3,1,2,4,3]'
+ *
+ * 给定一个含有 n 个正整数的数组和一个正整数 target 。
+ * 
+ * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr]
+ * ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：target = 4, nums = [1,4,4]
+ * 输出：1
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+ * 输出：0
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 1 
+ * 1 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 
+ * 如果你已经实现 O(n) 时间复杂度的解法, 请尝试设计一个 O(n log(n)) 时间复杂度的解法。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+  // 利用滑动窗口
+  let result = Number.MAX_SAFE_INTEGER
+  let i = 0; // 左边界
+  let sum = 0
+  for(let j = 0;j<nums.length;j++) {
+    sum += nums[j]
+    // 注意这里使用while，每次更新 i（起始位置），并不断比较子序列是否符合条件
+    while(sum >= target) { // 
+      const len = j - i + 1
+      result = Math.min(result, len)
+      sum -= nums[i++] // 变更i的起始位置
+    }
+  }
+  return result === Number.MAX_SAFE_INTEGER ? 0 : result
+};
+// @lc code=end
+
