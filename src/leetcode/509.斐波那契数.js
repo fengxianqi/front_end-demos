2ffe62d09457529da6090176bf9bbab3a7674474
fengxianqi <fengxianqi12@gmail.com>
2023-12-05 23:46:07 +0800

    feat: leetcode add 509 & 746

---
+++ src/leetcode/509.斐波那契数.js
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=javascript
+ *
+ * [509] 斐波那契数
+ *
+ * https://leetcode.cn/problems/fibonacci-number/description/
+ *
+ * algorithms
+ * Easy (66.01%)
+ * Likes:    716
+ * Dislikes: 0
+ * Total Accepted:    621.3K
+ * Total Submissions: 942.2K
+ * Testcase Example:  '2'
+ *
+ * 斐波那契数 （通常用 F(n) 表示）形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ * 
+ * 
+ * F(0) = 0，F(1) = 1
+ * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+ * 
+ * 
+ * 给定 n ，请计算 F(n) 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 3
+ * 输出：2
+ * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：n = 4
+ * 输出：3
+ * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 <= n <= 30
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+
+  if (n <= 1) {
+    return n;
+  }
+
+  const dp = [0, 1]
+  for (let i = 2; i < n; i++) {
+    const sum = dp[0] + dp[1]
+    dp[0] = dp[1]
+    dp[1] = sum 
+  }
+  return dp[1]
+};
+// @lc code=end
+
---
+++ src/leetcode/746.使用最小花费爬楼梯.js
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=746 lang=javascript
+ *
+ * [746] 使用最小花费爬楼梯
+ *
+ * https://leetcode.cn/problems/min-cost-climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (65.31%)
+ * Likes:    1369
+ * Dislikes: 0
+ * Total Accepted:    364K
+ * Total Submissions: 557.2K
+ * Testcase Example:  '[10,15,20]'
+ *
+ * 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+ * 
+ * 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+ * 
+ * 请你计算并返回达到楼梯顶部的最低花费。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：cost = [10,15,20]
+ * 输出：15
+ * 解释：你将从下标为 1 的台阶开始。
+ * - 支付 15 ，向上爬两个台阶，到达楼梯顶部。
+ * 总花费为 15 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：cost = [1,100,1,1,1,100,1,1,100,1]
+ * 输出：6
+ * 解释：你将从下标为 0 的台阶开始。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 2 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 4 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 6 的台阶。
+ * - 支付 1 ，向上爬一个台阶，到达下标为 7 的台阶。
+ * - 支付 1 ，向上爬两个台阶，到达下标为 9 的台阶。
+ * - 支付 1 ，向上爬一个台阶，到达楼梯顶部。
+ * 总花费为 6 。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 2 <= cost.length <= 1000
+ * 0 <= cost[i] <= 999
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+  
+  const dp = new Array(cost.length + 1)
+  dp[0] = 0
+  dp[1] = 0
+  for (let i = 2; i <= cost.length; i++) {
+    dp[i] = Math.min(dp[i - 1]+ cost[i - 1], dp[i - 2] + cost[i - 2])
+  }
+  return dp[cost.length]
+};
+// @lc code=end
+
